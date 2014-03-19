978b6ec6c1b918b18d9ff5809bf37b98528c62d1
AB244 <ajbenn04@louisville.edu>
2014-03-19 12:41:56 -0400

    Homepage Creation
    Creation, and formatting of home page, and about page almost complete.
    Adjustment of css file.

--- Pet Store/Pet Store/Controllers/HomeController.cs
+++ Pet Store/Pet Store/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Pet_Store.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "The following websites are cited:";
 
             return View();
         }
